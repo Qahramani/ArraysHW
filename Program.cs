c0aef5be52c18c0c97fe40c7a1a9202326365103
testtttttttttt1234 <wapigix358@bsomek.com>
2024-05-22 16:36:37 +0400

    Reversing array added :sparkles:

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ArraysHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Reversing Array
+            int[] numbers = { 2, 3, 4, 5, 6, 7 };
+
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                numbers[i] = numbers[i] + numbers[numbers.Length - 1 - i];
+                numbers[numbers.Length - 1 - i] = numbers[i] - numbers[numbers.Length - 1 - i];
+                numbers[i] = numbers[i] - numbers[numbers.Length - 1 - i];
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            #endregion
+        }
+    }
+}
