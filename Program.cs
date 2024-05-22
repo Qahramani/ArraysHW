492e3cba7d9995813c8e605e2ad6d859ef670e7c
testtttttttttt1234 <wapigix358@bsomek.com>
2024-05-22 17:08:43 +0400

    Second question added

--- Program.cs
+++ Program.cs
@@ -5,19 +5,36 @@
         static void Main(string[] args)
         {
             #region Reversing Array
-            int[] numbers = { 2, 3, 4, 5, 6, 7 };
+            //int[] numbers = { 2, 3, 4, 5, 6, 7 };
 
-            for (int i = 0; i < numbers.Length / 2; i++)
-            {
-                numbers[i] = numbers[i] + numbers[numbers.Length - 1 - i];
-                numbers[numbers.Length - 1 - i] = numbers[i] - numbers[numbers.Length - 1 - i];
-                numbers[i] = numbers[i] - numbers[numbers.Length - 1 - i];
-            }
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Console.Write(numbers[i] + " ");
-            }
+            //for (int i = 0; i < numbers.Length / 2; i++)
+            //{
+            //    numbers[i] = numbers[i] + numbers[numbers.Length - 1 - i];
+            //    numbers[numbers.Length - 1 - i] = numbers[i] - numbers[numbers.Length - 1 - i];
+            //    numbers[i] = numbers[i] - numbers[numbers.Length - 1 - i];
+            //}
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.Write(numbers[i] + " ");
+            //}
 
+            #endregion
+            #region MyRegion
+            int[] array1 = { 1, 2, 8 };
+            int[] array2 = { 4,3,2,5,1 };
+            int counter = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    if (array1[i] == array2[j])
+                         counter++;
+                }
+            }
+            if(counter == array1.Length)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
             #endregion
         }
     }
--- obj/Debug/net6.0/ArraysHW.AssemblyInfo.cs
+++ obj/Debug/net6.0/ArraysHW.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArraysHW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0aef5be52c18c0c97fe40c7a1a9202326365103")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArraysHW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArraysHW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
